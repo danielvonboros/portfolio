c578994e3ad32c57b27b53c64f15303e216c22a7
Daniel von Boros <danielvonboros@Daniels-MacBook-Pro.local>
2024-11-07 22:09:36 +0100

    feat: add Topbar for Navigation between pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from "./about/About";
 import Projects from "./projects/Projects";
 import Skills from "./skills/Skills";
 import Contact from "./contact/Contact";
+import { AppBar, Box, Button, Toolbar } from "@mui/material";
 
 const App = () => {
   const [currentSection, setCurrentSection] = useState("Home");
@@ -25,7 +26,24 @@ const App = () => {
     }
   };
 
-  return <About />;
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          {["Home", "About", "Projects", "Skills", "Contact"].map((section) => (
+            <Button
+              key={section}
+              color="inherit"
+              onClick={() => setCurrentSection(section)}
+            >
+              {section}
+            </Button>
+          ))}
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ flexGrow: 1, overflow: "auto", p: 3 }}>{renderSection()}</Box>
+    </>
+  );
 };
 
 export default App;
